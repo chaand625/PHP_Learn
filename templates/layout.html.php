292f90b25c8f39810f1f99c12f5975bf483e6396
bashac625 <bashac625@gmail.com>
2026-08-16 23:44:12 +0530

    added the templates in the public only for git commiting purpose and added jokes.php to get the users from db.

---
+++ templates/layout.html.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>$title</title>
+</head>
+<body>
+    <header>
+        <h1>Internet Joke Databse</h1>
+    </header>
+    
+
+    <nav>
+        <ul>
+            <li> <a href="index.php">Home</a> </li>
+            <li> <a href="jokes.php">Jokes List</a> </li>
+        </ul>
+    </nav>
+
+    <main>
+        <?= $output ?>
+    </main>
+
+    <footer>
+        &copy; IJDB 2026
+    </footer>
+</body>
+</html>
